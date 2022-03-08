ba5bb719ee5e171a2b860ee21a4e353dd4221619
ctulhu31 <ctulhu31@gmail.com>
2022-03-08 18:21:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,94 +1,62 @@
 import math
-import os
 
 class part:
-    def __init__(self, name, firstValue):
+    def __init__(self, name):
         self.__name = name
-        self.__firstValue = firstValue
-        self.__coefficient = 0
+        self.__val1 = None
+        self.__val2 = None
+        self.__coefficient = 0.0
 
-    def addNewValue(self, val):
+    def addValue(self, val, diff=1.0):
+        if self.__val1 is None:
+            self.__val1 = val
+        elif self.__val2 is None:
+            self.__val2 = val
+            self.__coefficient += math.fabs(self.__val2 - self.__val1) / math.fabs(diff)
+        else:
+            self.__val1 = self.__val2
+            self.__val2 = val
+            self.__coefficient += math.fabs(self.__val2 - self.__val1) / math.fabs(diff)
 
+    def getName(self, ):
+        return self.__name
 
+    def getCoefficient(self):
+        return str('%+10f' % self.__coefficient)
 
 def readdata(path):
-    data = open('path', 'r', encoding='utf-8')
-    datastring = data.read()
-    data.close()
+    data = open(path, 'r', encoding='utf-8')
+    xc = 0
+    datalist = []
+    res = []
+    for i in data:
+        z = i.replace('\n', '').split(' ')
+        if xc == 0:
+            for j in range(1, len(z)):
+                datalist.append(part(z[j]))
+            xc += 1
+        elif xc == 1:
+            res.append(float(z[0]))
+            for j in range(1, len(z)):
+                datalist[j - 1].addValue(float(z[j]))
+            xc += 1
+        else:
+            if len(res) == 2:
+                res[0] = res[1]
+                res[1] = float(z[0])
+            else:
+                res.append(float(z[0]))
+            for j in range(1, len(z)):
+                if not float(res[1] - res[0]) == 0:
+                    datalist[j - 1].addValue(float(z[j]), float(res[1] - res[0]))
+    return datalist
 
-
+def printresult(datalist):
+    for i in datalist:
+        print(i.getName() + ' ' + i.getCoefficient())
 
 def main():
     datapath = input('Enter path to data file: ')
-    readdata(datapath)
+    printresult(readdata(datapath))
 
-dx1k1 = 0
-dx2k1 = 0
-dx3k1 = 0
-dx4k1 = 0
-dx5k1 = 0
-dx6k1 = 0
-f = open('data.txt', 'r')
-i = 0
-line = f.readline()
-x11 = float(line.strip('\n'))
-line = f.readline()
-x12 = float(line.strip('\n'))
-line = f.readline()
-x13 = float(line.strip('\n'))
-line = f.readline()
-x14 = float(line.strip('\n'))
-line = f.readline()
-x15 = float(line.strip('\n'))
-line = f.readline()
-x16 = float(line.strip('\n'))
-
-line = f.readline()
-res1 = float(line.strip('\n'))
-try:
-    while line:
-        line = f.readline()
-        if float(line.strip('\n')) < 1000000000:
-            x21 = float(line.strip('\n'))
-            line = f.readline()
-            x22 = float(line.strip('\n'))
-            line = f.readline()
-            x23 = float(line.strip('\n'))
-            line = f.readline()
-            x24 = float(line.strip('\n'))
-            line = f.readline()
-            x25 = float(line.strip('\n'))
-            line = f.readline()
-            x26 = float(line.strip('\n'))
-            line = f.readline()
-            res2 = float(line.strip('\n'))
-            i = i + 1
-            razx1 = math.fabs(x21 - x11)
-            razx2 = math.fabs(x22 - x12)
-            razx3 = math.fabs(x23 - x13)
-            razx4 = math.fabs(x24 - x14)
-            razx5 = math.fabs(x25 - x15)
-            razx6 = math.fabs(x26 - x16)
-            razres = math.fabs(res2 - res1)
-            dx1k1 = dx1k1 + (razx1 / razres)
-            dx2k1 = dx2k1 + (razx2 / razres)
-            dx3k1 = dx3k1 + (razx3 / razres)
-            dx4k1 = dx4k1 + (razx4 / razres)
-            dx5k1 = dx5k1 + (razx5 / razres)
-            dx6k1 = dx6k1 + (razx6 / razres)
-            x11 = x21
-            x12 = x22
-            x13 = x23
-            x14 = x24
-            x15 = x25
-            x16 = x26
-            res1 = res2
-except:
-    print(line)
-f.close()
-print('%+20f' % dx1k1)
-print('%+20f' % dx2k1)
-print('%+20f' % dx3k1)
-print('%+20f' % dx4k1)
-print('%+20f' % dx5k1)
-print('%+20f' % dx6k1)
+main()
